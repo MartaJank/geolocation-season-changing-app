287ae6dbde7370768d3f43480b4533e7ea78e949
Marta Jankowska <martajan218@gmail.com>
2021-01-03 13:21:24 +0100

    added componentDidMount

--- src/App.js
+++ src/App.js
@@ -18,19 +18,27 @@ class App extends Component {
       latitude: null,
       errorMessage: "",
     };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ latitude: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
+      (position) => this.setState({ latitude: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.latitude}</div>;
+    return (
+      <div>
+        {this.state.errorMessage !== "" ? (
+          <p>Error: {this.state.errorMessage}</p>
+        ) : this.state.latitude ? (
+          <p>Latitude: {this.state.latitude}</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    );
   }
 }
 
